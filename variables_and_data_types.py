a4cfa6ba84246e6408e6c5779075245854ecb3c6
Rewon Shrestha <newa.rewon@gmail.com>
2025-11-26 07:24:08 +0545

    basic ideas of what is variables, data types and how to print values stores in variables

---
+++ variables_and_data_types.py
@@ -0,0 +1,11 @@
+name="Buddy" #strings
+age= 21 #intergers
+college_GPA= 3.10 #float
+
+print(name) #printing value store in "name" variable
+print(age) #printing value store in "age" variable
+print(college_GPA) #printing value store in "college_GPA" variable
+
+print("Hello World, I am", name) 
+print("I am currently", age, "yrs old.")
+print("And my college GPA is", college_GPA)
